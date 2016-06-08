c22e5ae36d5a67e91de977666910722f60ce54b8
8Torpedo <ciok.blazej@gmail.com>
2016-06-08 12:55:00 +0200

    Small changes to improve functionality

--- example/src/test.c
+++ example/src/test.c
@@ -5,29 +5,33 @@
 #include "board.h"
 
 enum {LED_RED, LED_GREEN, LED_BLUE};
+uint8_t testTable[] = { 2 , 2, 2, 2, 2};
+
 // test functions
+void testAverage(){
+	TEST_ASSERT_EQUAL(2, averageTemp(testTable, 5));
+}
 
 void testTempFalling(){
-	showTempChange(2, 1);
+	lastTemps(testTable, 3, 5, 3);
 	TEST_ASSERT_TRUE(Board_LED_Test(LED_BLUE));
 	TEST_ASSERT_FALSE(Board_LED_Test(LED_RED));
 	TEST_ASSERT_FALSE(Board_LED_Test(LED_GREEN));
 }
 
 void testTempRising(){
-	showTempChange(1, 2);
+	lastTemps(testTable, 1, 5, 3);
 	TEST_ASSERT_FALSE(Board_LED_Test(LED_BLUE));
 	TEST_ASSERT_FALSE(Board_LED_Test(LED_GREEN));
 	TEST_ASSERT_TRUE(Board_LED_Test(LED_RED));
 }
 
 void testTempStable(){
-	showTempChange(0, 0);
+	lastTemps(testTable, 2, 5, 3);
 	TEST_ASSERT_FALSE(Board_LED_Test(LED_RED));
 	TEST_ASSERT_TRUE(Board_LED_Test(LED_GREEN));
 	TEST_ASSERT_FALSE(Board_LED_Test(LED_BLUE));
 }
-
 
 int main(){
 
@@ -38,6 +42,7 @@ int main(){
 	RUN_TEST(testTempFalling);
 	RUN_TEST(testTempRising);
 	RUN_TEST(testTempStable);
+	RUN_TEST(testAverage);
 
 	return UNITY_END();
 }
