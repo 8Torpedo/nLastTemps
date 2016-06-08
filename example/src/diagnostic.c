432ea6eab55d342599fba7b014701fd79c1af0f2
8Torpedo <ciok.blazej@gmail.com>
2016-06-08 12:53:16 +0200

    Small changes to improve functionality

--- example/src/diagnostic.c
+++ example/src/diagnostic.c
@@ -9,26 +9,7 @@
 
 enum {LED_RED, LED_GREEN, LED_BLUE};
 
-void showTempChange(const uint8_t tempPrev, const uint8_t temp){
-
-	if(tempPrev==temp){
-		Board_LED_Set(LED_GREEN,TRUE);
-		Board_LED_Set(LED_BLUE,FALSE);
-		Board_LED_Set(LED_RED,FALSE);
-	}
-	else if(tempPrev>temp){
-		Board_LED_Set(LED_BLUE,TRUE);
-		Board_LED_Set(LED_GREEN,FALSE);
-		Board_LED_Set(LED_RED,FALSE);
-	}
-	else if(tempPrev<temp){
-		Board_LED_Set(LED_RED, TRUE);
-		Board_LED_Set(LED_BLUE,FALSE);
-		Board_LED_Set(LED_GREEN,FALSE);
-	}
-}
-
-int averageTempTable(uint8_t table[], const uint8_t TAB_SIZE){
+int averageTemp(uint8_t table[], const uint8_t TAB_SIZE){
 	uint8_t average = 0;
 	uint16_t sum = 0;
 	uint8_t index =0;
@@ -41,20 +22,40 @@ int averageTempTable(uint8_t table[], const uint8_t TAB_SIZE){
 	return average;
 }
 
-void lastTemps(uint8_t table[], uint8_t average, uint8_t TAB_SIZE){
-	TAB_SIZE-=3;
-	if ((table[TAB_SIZE++]>average)&&(table[TAB_SIZE++]>average)&&(table[TAB_SIZE++]>average)){
-		Board_LED_Set(LED_RED, TRUE);
-		Board_LED_Set(LED_BLUE,FALSE);
-		Board_LED_Set(LED_GREEN,FALSE);
-	} else if((table[TAB_SIZE++]<average)&&(table[TAB_SIZE++]<average)&&(table[TAB_SIZE++]<average)){
-		Board_LED_Set(LED_RED, FALSE);
-		Board_LED_Set(LED_BLUE,TRUE);
-		Board_LED_Set(LED_GREEN,FALSE);
-	} else{
-		Board_LED_Set(LED_RED, FALSE);
-		Board_LED_Set(LED_BLUE,FALSE);
-		Board_LED_Set(LED_GREEN,TRUE);
+void lastTemps(uint8_t table[], uint8_t average, uint8_t TAB_SIZE, const uint8_t index){
+	uint8_t rising=0;
+	uint8_t falling=0;
+	uint8_t stable=0;
+	uint8_t lastTAB_SIZE = TAB_SIZE - index;
+
+	while (lastTAB_SIZE<TAB_SIZE){
+		if(table[lastTAB_SIZE]<average){
+			falling++;
+		} else if(table[lastTAB_SIZE]>average){
+			rising++;
+		} else if(table[lastTAB_SIZE]==average){
+			stable++;
+		}
+		lastTAB_SIZE++;
+	}
+	if((falling>rising)&&(falling>stable)){
+		if(falling==index){
+			Board_LED_Set(LED_RED, FALSE);
+			Board_LED_Set(LED_BLUE,TRUE);
+			Board_LED_Set(LED_GREEN,FALSE);
+		}
+	} else if((rising>falling)&&(rising>stable)){
+		if(rising==index){
+			Board_LED_Set(LED_RED, TRUE);
+			Board_LED_Set(LED_BLUE,FALSE);
+			Board_LED_Set(LED_GREEN,FALSE);
+		}
+	} else if((stable>rising)&&(stable>falling)){
+		if(stable==index){
+			Board_LED_Set(LED_RED, FALSE);
+			Board_LED_Set(LED_BLUE,FALSE);
+			Board_LED_Set(LED_GREEN,TRUE);
+		}
 	}
 }
 
