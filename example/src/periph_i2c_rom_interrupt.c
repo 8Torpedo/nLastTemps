2add46004a2afe3bcee75b3ff6bea469c77c7057
8Torpedo <ciok.blazej@gmail.com>
2016-06-08 12:52:09 +0200

    Small changes to improve functionality

--- example/src/periph_i2c_rom_interrupt.c
+++ example/src/periph_i2c_rom_interrupt.c
@@ -1,4 +1,33 @@
-
+/*
+ * @brief I2C bus master example using the ROM interrupt API
+ *
+ * @note
+ * Copyright(C) NXP Semiconductors, 2013
+ * All rights reserved.
+ *
+ * @par
+ * Software that is described herein is for illustrative purposes only
+ * which provides customers with programming information regarding the
+ * LPC products.  This software is supplied "AS IS" without any warranties of
+ * any kind, and NXP Semiconductors and its licensor disclaim any and
+ * all warranties, express or implied, including all implied warranties of
+ * merchantability, fitness for a particular purpose and non-infringement of
+ * intellectual property rights.  NXP Semiconductors assumes no responsibility
+ * or liability for the use of the software, conveys no license or rights under any
+ * patent, copyright, mask work right, or any other intellectual property rights in
+ * or to any products. NXP Semiconductors reserves the right to make changes
+ * in the software without notification. NXP Semiconductors also makes no
+ * representation or warranty that such application will be suitable for the
+ * specified use without further testing or modification.
+ *
+ * @par
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation is hereby granted, under NXP Semiconductors' and its
+ * licensor's relevant copyrights in the software, without fee, provided that it
+ * is used in conjunction with NXP Semiconductors microcontrollers.  This
+ * copyright, permission, and disclaimer notice must appear in all copies of
+ * this code.
+ */
 #ifndef TEST
 
 #include "board.h"
@@ -321,7 +350,9 @@ int main(void)
 
 	sendI2CMaster(I2C_ADDR_7BIT, -1, 0x01, 0);
 	/* Toggle LED on other board via I2C */
+
 	const uint8_t TAB_SIZE = 10;
+	const uint8_t LAST_TEMPS = 4; // How many measures must be checked with average
 	uint8_t temp;
 	uint8_t table[TAB_SIZE];
 	uint8_t index = 0;
@@ -355,9 +386,9 @@ int main(void)
 			printf("Temp [%d] = %d\n", index, table[index]);
 			index++;
 			if (10==index){
-				average = averageTempTable(table, TAB_SIZE);
+				average = averageTemp(table, TAB_SIZE);
 				printf("Average: %d\n", average);
-				lastTemps(table, average, TAB_SIZE);
+				lastTemps(table, average, TAB_SIZE, LAST_TEMPS);
 				index=0;
 			}
 			break;
